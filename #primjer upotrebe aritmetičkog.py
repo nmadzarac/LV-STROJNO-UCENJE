e48aa1126c1ed998fdd435b3a132604802c791cb
nmadzarac <nikola.madzarac@gmail.com>
2025-03-04 13:24:44 +0100

    Add files via upload

---
+++ #primjer upotrebe aritmetičkog.py
@@ -0,0 +1,18 @@
+#Napišite program koji od korisnika zahtijeva unos radnih sati te koliko je plaćen po radnom satu. Koristite ugrađenu
+#Python metodu input(). Nakon toga izračunajte koliko je korisnik zaradio i ispišite na ekran. Na kraju prepravite
+#rješenje na način da ukupni iznos izračunavate u zasebnoj funkciji naziva total_euro.
+
+def total_euro():
+    c = a * b
+    c = float(c)
+    return c
+
+print('Unesi broj radnih sati : ')
+a = input()
+a = float(a)
+print('Unesi satnicu : ')
+b = input()
+b = float(b)
+d = total_euro()
+print('Ukupno :')
+print(d)
